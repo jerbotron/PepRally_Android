5c71caadf9b94be3957eb6b091db2a564a305eab
Jeremy Wang <jer.wang25@gmail.com>
2016-03-25 16:29:33 -0500

    Update testPython.py

--- scripts/testPython.py
+++ scripts/testPython.py
@@ -1,4 +1,4 @@
-import boto3, json, urllib, urllib2
+import json, urllib, urllib2#, boto3
 from teamParsers import *
 
 def createTable():
@@ -31,10 +31,10 @@ def createTable():
     )
     return table
 
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
+# dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
 
 # table = createTable()
-table = dynamodb.Table("PlayerProfiles")
+# table = dynamodb.Table("PlayerProfiles")
 
 rosters_urls = {
 "baseball": "http://texassports.com/roster.aspx?path=baseball",
@@ -53,14 +53,19 @@ rosters_urls = {
 "wvball": "http://www.texassports.com/roster.aspx?path=wvball"
 }
 
-js = urllib2.urlopen(rosters_urls["football"])
-team = "football"
+js = urllib2.urlopen(rosters_urls["xc_tf"])
+team = "xc_tf"
 
 print "Team = " + team
+table = None
 
 # parse_wsoc(table, js, team)
 # parse_baseball(table, js, team)
 # parse_basketball(table, js, team)
-parse_football(table, js, team)
+# parse_football(table, js, team)
+# parse_mgolf(table, js, team);
+# parse_mswim(table, js, team);
+# parse_mten(table, js, team);
+parse_xc_tf(table, js, team);
 
-print("Table status:", table.table_status)
+# print("Table status:", table.table_status)
