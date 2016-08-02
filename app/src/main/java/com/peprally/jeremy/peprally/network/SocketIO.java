c7ca1239c65ce0a4ade026de5fea46fdc128d6ba
Jeremy <jer.wang25@gmail.com>
2016-08-01 21:39:35 -0500

    finished part 2 of messaging feature, added nodejs socket io server

---
+++ app/src/main/java/com/peprally/jeremy/peprally/network/SocketIO.java
@@ -0,0 +1,8 @@
+package com.peprally.jeremy.peprally.network;
+
+/**
+ * Created by jeremy on 31/07/16.
+ */
+
+public class SocketIO {
+}
---
+++ app/src/main/java/com/peprally/jeremy/peprally/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.peprally.jeremy.peprally.utils;
+
+/**
+ * Created by jeremy on 31/07/16.
+ */
+
+public class Constants {
+}
