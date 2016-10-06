ac6b3236e06e506fce87594055b345abfd41ed3c
Jeremy <jer.wang25@gmail.com>
2016-10-06 18:51:56 -0500

    merged all messaging/notification servers to peprallyapp.co

--- app/src/main/java/com/peprally/jeremy/peprally/utils/Constants.java
+++ app/src/main/java/com/peprally/jeremy/peprally/utils/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
     // Amazon Web Services: EC2 Server Constants
 //    private static final String pushServerURL = "http://peprally-push.dcif4cvzmx.us-west-2.elasticbeanstalk.com/send";
 //    public static final String PUSH_SERVER_URL = "http://ec2-107-21-196-112.compute-1.amazonaws.com/send";
-    public static final String PUSH_SERVER_URL = "http://ec2-52-90-150-67.compute-1.amazonaws.com/send";
-    public static final String SOCKETIO_SERVER_URL = "http://ec2-52-90-150-67.compute-1.amazonaws.com";
+    public static final String PUSH_SERVER_URL = "http://ec2-54-167-226-181.compute-1.amazonaws.com/push";
+    public static final String SOCKETIO_SERVER_URL = "http://ec2-54-167-226-181.compute-1.amazonaws.com";
 
     public final static String IDENTITY_POOL_ID = "us-east-1:62a77974-d33d-4131-8a1d-122db8e07dfa";
     public final static Regions COGNITO_REGION = Regions.US_EAST_1;
